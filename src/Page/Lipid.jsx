744a13ec5751951a60f7846d0c7f379dec2df648
Ahnaf Hasan Shifat <sheikhahnafshifat@gmail.com>
2024-01-24 19:04:55 +0600

    Update prediction value in Lipid page with data from data.pred

--- src/Page/Lipid.jsx
+++ src/Page/Lipid.jsx
@@ -130,7 +130,7 @@ function Lipid() {
                 <div className="w-full h-full flex flex-col items-center p-2">
                   <h1 className="bg-violet-500 text-gray-100 mt-2 p-2 px-4 text-lg rounded shadow font-mono">
                     Prediction Value:{" "}
-                    <span className="text-white font-semibold">20.34</span>
+                    <span className="text-white font-semibold">{data.pred}</span>
                   </h1>
                   {data.graph ? (
                     <img
